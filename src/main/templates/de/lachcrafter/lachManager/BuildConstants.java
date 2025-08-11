58542d11deeca7d8b49b68505871474af7081217
LachCrafter <lachcrafteryt@gmail.com>
2025-08-11 11:49:57 +0200

    fix(BuildConstants): missing semicolon

--- src/main/templates/de/lachcrafter/lachManager/BuildConstants.java
+++ src/main/templates/de/lachcrafter/lachManager/BuildConstants.java
@@ -5,5 +5,5 @@ public class BuildConstants {
 
     public static final String VERSION = "${version}";
 
-    public static final boolean IN_DEVELOPMENT = "${in_development}"
+    public static final boolean IN_DEVELOPMENT = "${in_development}";
 }
